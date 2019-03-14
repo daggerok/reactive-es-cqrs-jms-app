f0b8bcbf2a25190b04c1eae23b9909d4c089e519
Maksim Kostromin <daggerok@gmail.com>
2019-03-15 01:06:44 +0200

    Refactor little bit..

--- src/main/kotlin/com/github/daggerok/cqrses/escqrs/ReactiveJmsEsCqrsApp.kt
+++ src/main/kotlin/com/github/daggerok/cqrses/escqrs/ReactiveJmsEsCqrsApp.kt
@@ -6,14 +6,15 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.io.ClassPathResource
-import org.springframework.http.MediaType.APPLICATION_STREAM_JSON
 import org.springframework.http.MediaType.TEXT_EVENT_STREAM
 import org.springframework.jms.core.JmsTemplate
+import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.body
 import org.springframework.web.reactive.function.server.router
 import reactor.core.publisher.Flux
 import reactor.core.publisher.toMono
 import reactor.core.scheduler.Schedulers
+import java.net.URI
 
 @Configuration
 class RestApi(val sharedStream: Flux<Map<String, String>>,
@@ -23,24 +24,25 @@ class RestApi(val sharedStream: Flux<Map<String, String>>,
     resources("/", ClassPathResource("/classpath:/static/"))
     //contentType(org.springframework.http.MediaType.APPLICATION_JSON)
     GET("/event-stream") {
-      ok().contentType(APPLICATION_STREAM_JSON)
+      ok()//.contentType(org.springframework.http.MediaType.APPLICATION_STREAM_JSON)
           .contentType(TEXT_EVENT_STREAM)
           .body(sharedStream)
           .subscribeOn(Schedulers.elastic())
     }
+    fun ServerRequest.baseUrl() = "${this.uri().scheme}://${this.uri().authority}"
     POST("/**") {
-      ok().body(it.bodyToMono(Object::class.java)
+      created(URI.create("${it.baseUrl()}/event-stream"))
+          .body(it.bodyToMono(Object::class.java)
           .map { jmsTemplate.convertAndSend(REACTIVE_QUEUE, it) }
+          .then("Message sent.".toMono())
           .subscribeOn(Schedulers.elastic()))
     }
     path("/**") {
-      val uri = it.uri()
-      val basePath = "${uri.scheme}://${uri.authority}"
       ok().body(mapOf(
           "_links" to listOf(
               mapOf(
                   "rel" to "_self",
-                  "href" to basePath + uri.path,
+                  "href" to it.baseUrl() + it.path(),
                   "templated" to false
               )
           )
@@ -50,8 +52,8 @@ class RestApi(val sharedStream: Flux<Map<String, String>>,
 }
 
 @SpringBootApplication
-class FbEsCqrsAppApplication
+class ReactiveJmsEsCqrsApp
 
 fun main(args: Array<String>) {
-  runApplication<FbEsCqrsAppApplication>(*args)
+  runApplication<ReactiveJmsEsCqrsApp>(*args)
 }
